02fde46713c97d53291bc599af1cf256bb629faf
Yabl04ko23 <71548049+Yabl04ko23@users.noreply.github.com>
2020-10-03 23:20:04 +0300

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,7 @@
 const items = document.querySelectorAll(".item");
 console.log("В списке " + items.length + " категории.");
 
-for (let i=0; i<items.length; i++) {;
+for (let i=0; i<items.length; i++) {
     console.log("Категория: " + items[i].children[0].textContent);
     console.log("Количество элементов: " + items[i].children[1].children.length);
 }
